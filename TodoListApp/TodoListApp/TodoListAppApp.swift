e287682c6aaca75c9c62f7f8a0ec11474bd97659
Carlos Morgado <cmorgadoalarcon@gmail.com>
2025-07-30 16:54:09 +0200

    * First files

---
+++ TodoListApp/TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Carlos Morgado on 30/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
