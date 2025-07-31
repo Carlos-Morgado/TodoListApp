a238ec30df118176fe0d7d040c276f418684dec1
Carlos Morgado <cmorgadoalarcon@gmail.com>
2025-07-31 17:34:06 +0200

    * Creating folders, task view model + task model

---
+++ TodoListApp/TodoListApp/Model/TaskModel.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  TodoListApp
+//
+//  Created by Carlos Morgado on 31/7/25.
+//
+
+import Foundation
+
+struct TaskModel: Codable {
+    
+}
---
+++ TodoListApp/TodoListApp/ViewModel/NoteViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  NoteViewModel.swift
+//  TodoListApp
+//
+//  Created by Carlos Morgado on 31/7/25.
+//
+
+import Foundation
+import SwiftUI
+
+class TaskViewModel: ObservableObject {
+   
+}
